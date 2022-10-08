248986b8577f69364e4c8b831c39670efef7f821
AFRIAULAR <aularafrica@gmail.com>
2022-10-08 03:45:32 -0300

    min & max characters

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 const $ = (selector) => document.querySelector(selector)
 const $$ = (selectors) => document.querySelectorAll(selectors)
 
+let length = document.getElementById('lengthChar');
 let letters = $('#onlyLetters')
 let numeric = $('#alphaNumeric')
 let allChar = $('#allChar')
@@ -8,28 +9,25 @@ let upperCase = $('#upperCase')
 let lowerCase = $('#lowerCase')
 
 let alphabet = "abcdefghijklmnopqrstuvwxyz"
-//const arrayAlphabet = Array.from(alphabet)
-//console.log(arrayAlphabet)
+const arrayAlphabet = Array.from(alphabet)
+
 let upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-//const arrayAlphabetUpper = Array.from(upperLetters)
-//console.log(arrayAlphabetUpper)
+const arrayAlphabetUpper = Array.from(upperLetters)
+
 let numbers = "0123456789"
-//const arrayNumbers = Array.from(numbers)
-//console.log(arrayNumbers)
+const arrayNumbers = Array.from(numbers)
+
 let symbol = ".?,;-_¡!¿*%&$/"
-//const arraySymbol = Array.from(symbol)
-//console.log(arraySymbol)
-//let length = $('#lengthChar')
-const form = $('#formContainer')
-let length = document.getElementById('lengthChar');
-
-
+const arraySymbol = Array.from(symbol)
 
 const generatePassword = (alphabet, length) => {
     let password = "";
-    for (let i = 0; i < length; i++) {
+    if(length > 5 && length <13) {
+        for (let i = 0; i < length; i++) {
         let random = Math.floor(Math.random() * alphabet.length);
         password += alphabet.charAt(random);
+    }
+//falta agregar msg de error
     }
     return password
 }
