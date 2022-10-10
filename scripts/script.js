9dec54c90715cd76baa75a7e170fd42589d8bfa7
AFRIAULAR <aularafrica@gmail.com>
2022-10-10 20:51:23 -0300

    add custom events

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 const $ = (selector) => document.querySelector(selector)
 const $$ = (selectors) => document.querySelectorAll(selectors)
 
+const button = $('#generatePassword')
 let length = document.getElementById('lengthChar');
 let letters = $('#onlyLetters')
 let numeric = $('#alphaNumeric')
@@ -8,46 +9,63 @@ let allChar = $('#allChar')
 let upperCase = $('#upperCase')
 let lowerCase = $('#lowerCase')
 
-let alphabet = "abcdefghijklmnopqrstuvwxyz"
-const arrayAlphabet = Array.from(alphabet)
-
+let lowerLetters = "abcdefghijklmnopqrstuvwxyz"
 let upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const arrayAlphabetUpper = Array.from(upperLetters)
-
 let numbers = "0123456789"
-const arrayNumbers = Array.from(numbers)
+let symbol = ".?;-_¡!¿*%&$/"
+let selectedOptions = []
 
-let symbol = ".?,;-_¡!¿*%&$/"
-const arraySymbol = Array.from(symbol)
+// CUSTOM EVENTS
+upperCase.addEventListener("click", (e) => {
+    (upperCase.checked) ? selectedOptions.push(upperLetters) :
+        selectedOptions.splice(selectedOptions.indexOf(upperLetters), 1);
+})
 
-const generatePassword = (alphabet, length) => {
-    let password = "";
-    if(length > 5 && length <13) {
-        for (let i = 0; i < length; i++) {
-        let random = Math.floor(Math.random() * alphabet.length);
-        password += alphabet.charAt(random);
-    }
-//falta agregar msg de error
-    }
-    return password
-}
+lowerCase.addEventListener("click", (e) => {
+    (lowerCase.checked) ? selectedOptions.push(lowerLetters) :
+    selectedOptions.splice(selectedOptions.indexOf(lowerLetters), 1);
 
-const generate = () => {
-    let length = lengthChar.value    
+})
+
+numeric.addEventListener("click", (e) => {
+    (numeric.checked) ? selectedOptions.push(numbers) :
+        selectedOptions.splice(selectedOptions.indexOf(numbers), 1)
+})
+
+allChar.addEventListener("click", (e) => {
+    (allChar.checked) ? selectedOptions.push(symbol) :
+        selectedOptions.splice(selectedOptions.indexOf(symbol), 1)
+
+})
+
+// const generatePassword = (alphabet, length) => {
+//     let password = "";
+//     if(length > 5 && length <13) {
+//         for (let i = 0; i < length; i++) {
+//         let random = Math.floor(Math.random() * alphabet.length);
+//         password += alphabet.charAt(random);
+//     }
+// //falta agregar msg de error
+//     }
+//     return password
+// }
+
+// const generate = () => {
+//     let length = lengthChar.value    
     
-    if (letters.checked) alphabet;
-    if (numeric.checked) alphabet += numbers;
-    if (allChar.checked) alphabet += numbers += symbol;
-    if (upperCase.checked) alphabet += upperLetters;
-    if (lowerCase.checked) alphabet;
+//     if (letters.checked) alphabet;
+//     if (numeric.checked) alphabet += numbers;
+//     if (allChar.checked) alphabet += numbers += symbol;
+//     if (upperCase.checked) alphabet += upperLetters;
+//     if (lowerCase.checked) alphabet;
 
-    const passGenerated = $('#passGenerated')
-    passGenerated.innerText = generatePassword(alphabet, length)
+//     const passGenerated = $('#passGenerated')
+//     passGenerated.innerText = generatePassword(alphabet, length)
 
-}
+// }
     
-// EVENT
-const button = $('#generatePassword')
+
+// EVENT GENERATE
 button.addEventListener("click", () => {
      generate();
 });
